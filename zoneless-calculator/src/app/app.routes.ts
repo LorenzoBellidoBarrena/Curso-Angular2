571f81321a6a09db87abb7ee69b9b070ebba09f0
Bellido Barrena <lorenzo-jesus.bellido@getronics.com>
2025-03-25 15:45:06 +0100

    Seccion 2 terminada

--- zoneless-calculator/src/app/app.routes.ts
+++ zoneless-calculator/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'calculator',
+        //loadComponent: () => import('./calculator/views/calculator-view/calculator-view.component').then(m => m.CalculatorViewComponent)
+        loadComponent: () => import('@/calculator/views/calculator-view/calculator-view.component') // Al poner export default en el componente
+    },
+    {
+        path: '**',
+        redirectTo: 'calculator'
+    }
+];
