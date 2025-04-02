2463b8592aaa831207447ffd73f4095d3e4c11f7
Bellido Barrena <lorenzo-jesus.bellido@getronics.com>
2025-04-02 09:40:31 +0200

    Fin sección 11

--- github-issues/src/app/modules/issues/pages/issue/issue-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-issue-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './issue-page.component.html',
-})
-export default class IssuePageComponent { }
--- github-issues/src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-issues-list-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './issues-list-page.component.html',
-})
-export default class IssuesListPageComponent { }
